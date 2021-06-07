bedb1983f4dda16bc691d054c543c35119160d7d
Jesús <jhdzmar@gmail.com>
2021-06-07 02:37:10 +0200

    Use schedule to check for plots

--- processor/processor.go
+++ processor/processor.go
@@ -15,6 +15,9 @@ type Processor struct {
 	// client is the ChiaFactory API client
 	client *client.Client
 
+	// reporter will paint the current status of the Processor to stdout
+	reporter *Reporter
+
 	// plots is the list of plots the processor is working with
 	plots []*plot.Plot
 
@@ -26,11 +29,27 @@ type Processor struct {
 
 	// plotDir is where plots will be downloaded to
 	plotDir string
+
+	// schedule tells us when to check for plots
+	schedule map[string]time.Time
 }
 
 func (proc *Processor) process(ctx context.Context) error {
 	for i := range proc.plots {
 		p := proc.plots[i]
+
+		// if it's not
+		s, ok := proc.schedule[p.ID]
+
+		// if it's not here, it means we don't have to check any longer
+		if !ok {
+			continue
+		}
+
+		// only check if we're past the scheduled time
+		if s.After(time.Now()) {
+			continue
+		}
 
 		// always reload
 		newP, err := proc.client.GetPlot(ctx, p.ID)
@@ -45,36 +64,43 @@ func (proc *Processor) process(ctx context.Context) error {
 			p.UpdatePlottingProgress(newP.PlottingProgress)
 		}
 
+		// by default we'll check every 10 minutes
+		proc.schedule[p.ID] = s.Add(10 * time.Minute)
+
 		switch p.State {
 		case plot.StatePending:
 			log.Debug("%s has not started plotting", p)
 		case plot.StatePlotting:
 			log.Debug("%s is currently being plotted (progress=%d%%)", p, newP.PlottingProgress)
 		case plot.StatePublished:
+			proc.schedule[p.ID] = s.Add(5 * time.Second)
 			switch p.DownloadState {
 			case plot.DownloadStateNotStarted:
 				proc.downloads.Add(1)
 				go func() {
 					defer proc.downloads.Done()
-					p.Download(ctx, proc.plotDir)
+					p.Download(ctx, proc.plotDir, []string{})
 				}()
 			case plot.DownloadStateDownloading:
 				log.Debug("%s is still being downloaded", p)
 			case plot.DownloadStateFailed:
 				log.Debug("%s download has failed", p)
+				delete(proc.schedule, p.ID)
 			case plot.DownloadStateDownloaded:
 				proc.client.DeletePlot(ctx, p.ID)
+				delete(proc.schedule, p.ID)
 			default:
 				return fmt.Errorf("unexpected download state (%s)", p.DownloadState)
 			}
 		case plot.StateCancelled, plot.StateExpired:
+			delete(proc.schedule, p.ID)
 			log.Debug("%s is expired or cancelled", p)
 		default:
 			return fmt.Errorf("unexpected state (%s)", p.State)
 		}
 	}
 
-	writeReport(proc.plots)
+	proc.reporter.render(proc.plots)
 
 	return nil
 }
@@ -93,6 +119,10 @@ func (proc *Processor) Start(ctx context.Context, orderID string) (chan struct{}
 	}
 
 	proc.plots = plots
+
+	for _, plot := range plots {
+		proc.schedule[plot.ID] = time.Now()
+	}
 
 	log.Infof("%s has %d plots", order, len(plots))
 
@@ -128,12 +158,14 @@ func (proc *Processor) Start(ctx context.Context, orderID string) (chan struct{}
 	return done, nil
 }
 
-func NewProcessor(c *client.Client, plotDir string, frequency time.Duration) (*Processor, error) {
+func NewProcessor(c *client.Client, r *Reporter, plotDir string, frequency time.Duration) (*Processor, error) {
 	p := &Processor{
 		client:    c,
+		reporter:  r,
 		downloads: sync.WaitGroup{},
 		frequency: frequency,
 		plotDir:   plotDir,
+		schedule:  map[string]time.Time{},
 	}
 	return p, nil
 }
