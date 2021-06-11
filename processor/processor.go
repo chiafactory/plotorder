270cb0019e590deabc264aff85720530526ebb4b
Jesús <jhdzmar@gmail.com>
2021-06-11 02:48:23 +0200

    Only update schedule when necessary

--- processor/processor.go
+++ processor/processor.go
@@ -125,15 +125,17 @@ func (proc *Processor) process(ctx context.Context) error {
 
 		// only reload based on the schedule
 		var (
-			newP = p
-			err  error
-			now  = time.Now()
+			newP           = p
+			err            error
+			updateSchedule = false
+			now            = time.Now()
 		)
 		if s.Before(now) {
 			newP, err = proc.client.GetPlot(ctx, p.ID)
 			if err != nil {
 				return err
 			}
+			updateSchedule = true
 		}
 
 		// update state and progres if necessary
@@ -146,18 +148,18 @@ func (proc *Processor) process(ctx context.Context) error {
 		}
 
 		// by default we'll retrieve the plots from the API every 10 minutes
-		proc.schedule[p.ID] = now.Add(10 * time.Minute)
+		nextScheduleTime := now.Add(10 * time.Minute)
 
 		switch p.State {
 		case plot.StatePending:
 			log.Debugf("%s has not started plotting", p)
 		case plot.StatePlotting:
-			proc.schedule[p.ID] = now.Add(1 * time.Minute)
+			nextScheduleTime = now.Add(1 * time.Minute)
 			log.Debugf("%s is currently being plotted (progress=%d%%)", p, newP.PlottingProgress)
 		case plot.StatePublished:
 			switch p.DownloadState {
 			case plot.DownloadStateNotStarted:
-				proc.schedule[p.ID] = now.Add(2 * time.Second)
+				nextScheduleTime = now.Add(2 * time.Second)
 				plotDir, err := proc.getPlotDownloadDirectory(p)
 				if err != nil {
 					return err
@@ -168,7 +170,7 @@ func (proc *Processor) process(ctx context.Context) error {
 			case plot.DownloadStatePreparing:
 				log.Debugf("%s is being prepared for download", p)
 			case plot.DownloadStateReady:
-				proc.schedule[p.ID] = now.Add(10 * time.Minute)
+				nextScheduleTime = now.Add(10 * time.Minute)
 				proc.downloads.Add(1)
 				go func() {
 					defer proc.downloads.Done()
@@ -194,7 +196,11 @@ func (proc *Processor) process(ctx context.Context) error {
 			return fmt.Errorf("unexpected state (%s)", p.State)
 		}
 
-		log.Debugf("%s will be checked again at %s", p, proc.schedule[p.ID])
+		if updateSchedule {
+			proc.schedule[p.ID] = nextScheduleTime
+			log.Debugf("%s will be checked again at %s", p, nextScheduleTime)
+		}
+
 	}
 
 	proc.reporter.render(proc.plots)
